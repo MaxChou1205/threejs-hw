9687fbb420a7c707432a8c1a3464f30bec823de4
Max.Chou <v2asault@yahoo.com.tw>
2023-10-09 23:43:32 +0800

    add mouse event and canvas element

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import fragmentShader from "./shaders/fragment.glsl";
 import atmosphereVertex from "./shaders/atmosphereVertex.glsl";
 import atmosphereFragment from "./shaders/atmosphereFragment.glsl";
 
+const canvasContainer = document.querySelector("#canvas") as HTMLCanvasElement;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -13,11 +14,11 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer({
-  antialias: true
+  antialias: true,
+  canvas: document.querySelector("#canvas") as HTMLCanvasElement
 });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild(renderer.domElement);
 
 // create sphere
 const geometry = new THREE.SphereGeometry(5, 50, 50);
@@ -40,7 +41,7 @@ const material = new THREE.ShaderMaterial({
 });
 const sphere = new THREE.Mesh(geometry, material);
 
-scene.add(sphere);
+// scene.add(sphere);
 
 // create atmosphere
 const atmosphereGeometry = new THREE.SphereGeometry(5, 50, 50);
@@ -56,11 +57,46 @@ scene.add(atmosphere);
 
 camera.position.z = 10;
 
+const group = new THREE.Group();
+group.add(sphere);
+scene.add(group);
+
+const starGeometry = new THREE.BufferGeometry();
+const starMaterial = new THREE.PointsMaterial({ color: 0xffffff });
+const starVertices = [];
+for (let i = 0; i < 10000; i++) {
+  const x = (Math.random() - 0.5) * 2000;
+  const y = (Math.random() - 0.5) * 2000;
+  const z = -Math.random() * 2000;
+  starVertices.push(x, y, z);
+}
+starGeometry.setAttribute(
+  "position",
+  new THREE.Float32BufferAttribute(starVertices, 3)
+);
+const stars = new THREE.Points(starGeometry, starMaterial);
+scene.add(stars);
+
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
 function animate() {
   requestAnimationFrame(animate);
 
   renderer.render(scene, camera);
   sphere.rotation.y += 0.01;
+  group.rotation.x = mouse.y * 2;
+  group.rotation.y = mouse.x * 0.5;
 }
 
 animate();
+
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / window.innerWidth) * 2
+  mouse.y = (event.clientY / window.innerHeight) * 2 + 1
+  console.log(mouse.x, mouse.y);
+
+})
+
